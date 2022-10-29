a8bc15e597b011ebdaea5b73522daa984508f37b
kavyahallur123 <kavyahallur199@gmail.com>
2022-10-29 12:30:35 +0530

    Added : checking sample email ids

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,11 +4,12 @@ public class UserRegistration {
     public static void main(String[] args) {
         // Create object of class
         ValidateUserInfo validateUserInfo = new ValidateUserInfo();
-        // Method calling to check Firstname
+        // Method calling to check Firstname,lastname, emailId, mobilenumber, password
         validateUserInfo.checkFirstName();
         validateUserInfo.checkLastName();
         validateUserInfo.checkEmail();
         validateUserInfo.checkMobileNumber();
         validateUserInfo.checkPassword();
+        validateUserInfo.checkSampleEmail();
     }
 }
