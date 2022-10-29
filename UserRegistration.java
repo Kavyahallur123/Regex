c1dfdc59c6240766efb47db5894c5b5b7b5cae7c
kavyahallur123 <kavyahallur199@gmail.com>
2022-10-29 12:04:29 +0530

    Added: Lastname validation program

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,12 +4,8 @@ public class UserRegistration {
     public static void main(String[] args) {
         // Create object of class
         ValidateUserInfo validateUserInfo = new ValidateUserInfo();
-        // Method calling to check Firstname,lastname, emailId, mobilenumber, password
+        // Method calling to check Firstname
         validateUserInfo.checkFirstName();
         validateUserInfo.checkLastName();
-        validateUserInfo.checkEmail();
-        validateUserInfo.checkMobileNumber();
-        validateUserInfo.checkPassword();
-        validateUserInfo.checkSampleEmail();
     }
 }
